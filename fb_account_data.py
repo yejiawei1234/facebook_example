fc5afd1d206d94b014a0def363941963d4ef90b4
yejiawei1234 <yejiawei123@gmail.com>
2018-07-19 14:10:04 +0800

    add deal_insight func

--- fb_account_data.py
+++ fb_account_data.py
@@ -23,7 +23,6 @@ config_filename = os.path.join(this_dir, 'config.json')
 with open(config_filename) as config_file:
     config = json.load(config_file)
 
-## Setup session and api objects
 session = FacebookSession(
    config['app_id'],
    config['app_secret'],
@@ -34,117 +33,43 @@ start_date = (date.today() - timedelta(days=7)).strftime('%Y-%m-%d')
 end_date = (date.today() - timedelta(days=2)).strftime('%Y-%m-%d')
 
 
-# def get_campaign_data(account, start_date, end_date):
-#     input_data_list = []
-#     for campaign in account.get_campaigns(fields=[AdCampaign.Field.name]):
-#         my_insights = campaign.get_insights(fields=[
-#                                                     'campaign_id',
-#                                                     'campaign_name',
-#                                                     'impressions',
-#                                                     'clicks',
-#                                                     'spend',
-#                                                     'actions',
-#                                                     'ctr'
-#                                                     ],
-#                                             params= {'time_range': {'since': start_date,
-#                                                                     'until': end_date},
-#                                                      'time_increment': 1})
-#
-#         for stat in my_insights:
-#             data_dict = {}
-#             for statfield in stat:
-# #                print("\t%s:\t\t%s" % (statfield, stat[statfield]))
-#                 if statfield != 'actions':
-#                     data_dict[statfield] = stat[statfield]
-#                     print(statfield)
-#                 else:
-#                     for i in stat[statfield]:
-#                         if i.get('action_type') == 'mobile_app_install':
-#                             data_dict[statfield] = i.get('value')
-#             input_data_list.append(data_dict)
-#     return input_data_list
-#
-# def deal_df(input_data_list, outputdir, filename):
-#     out_put_df = pd.DataFrame(input_data_list)
-#     columns = out_put_df.columns
-#
-#     if 'spend' in columns:
-#         out_put_df['spend'] = out_put_df['spend'].astype(float)
-#     if 'actions' in columns:
-#         out_put_df['actions'] = out_put_df['actions'].astype(float)
-#     if 'clicks' in columns:
-#         out_put_df['clicks'] = out_put_df['clicks'].astype(int)
-#     if 'impressions' in columns:
-#         out_put_df['impressions'] = out_put_df['impressions'].astype(int)
-#     if 'ctr' in columns:
-#         out_put_df['ctr'] = out_put_df['ctr'].astype(float)
-#     out_put_df = out_put_df[['campaign_id','campaign_name','date_start','date_stop','impressions','clicks','ctr','actions','spend']]
-#     out_put_df.to_excel(f'{outputdir}/{filename}.xlsx', index=False)
-#
-#
-# def output_folder(output):
-#     if not os.path.isabs(output):
-#         home_path = os.path.expanduser('~/Desktop')
-#         output_folder_path = os.path.join(home_path, output)
-#         if os.path.exists(output_folder_path):
-#             print(output_folder_path)
-#             return output_folder_path
-#         else:
-#             print('the output folder is not existed, creating the folder now')
-#             os.mkdir(output_folder_path)
-#             return output_folder_path
-#     else:
-#         if os.path.exists(output):
-#             print('-------', output)
-#             return output
-#         else:
-#             print('the output folder is not existed, creating the folder now')
-#             os.mkdir(output)
-#             return output
-#
-#
-# @click.command()
-# @click.option('--output', '-o', default='output', help='The output folder')
-# @click.option('--start', '-s', default=start_date, help='The start date')
-# @click.option('--end', '-e', default=end_date, help='The end date')
-# @click.option('--filename', '-f', default='test', help='The output filename')
-# def main(output, start, end, filename):
-#     my_account = AdAccount(config['act_id'])
-#     output_folder_path = output_folder(output)
-#     df_list = get_campaign_data(my_account, start, end)
-#     deal_df(df_list, output_folder_path, filename)
-    
+def deal_insight(insight):
+    data_dict = {}
+    for stat in insight:
+        for statfield in stat:
+            if statfield != 'actions':
+                data_dict[statfield] = stat[statfield]
+            else:
+                for i in stat[statfield]:
+                    if i.get('action_type') == 'mobile_app_install':
+                        data_dict[statfield] = i.get('value')
+    return data_dict
+
 
 if __name__ == '__main__':
     FacebookAdsApi.set_default_api(api)
-    ### Setup user and read the object from the server
     me = AdUser(fbid='me')
     input_data_list = []
     my_accounts_list = list(me.get_ad_accounts())
     for account in my_accounts_list:
-        account_insight = account.get_insights(fields=[
-            'account_id',
-            'account_name',
-            'impressions',
-            'clicks',
-            'spend',
-            'ctr',
-            'actions',
-        ],
-        params={'date_preset': 'yesterday'})
-        for stat in account_insight:
-            data_dict = {}
-            for statfield in stat:
-                if statfield != 'actions':
-                    data_dict[statfield] = stat[statfield]
-                else:
-                    for i in stat[statfield]:
-                        if i.get('action_type') == 'mobile_app_install':
-                            data_dict[statfield] = i.get('value')
+        account_insight = account.get_insights(fields=['account_id',
+                                                       'account_name',
+                                                       'impressions',
+                                                       'clicks',
+                                                       'spend',
+                                                       'ctr',
+                                                       'actions'],
+                                                params={'date_preset': 'yesterday'})
+        data_dict = deal_insight(account_insight)
+        if data_dict:
             input_data_list.append(data_dict)
 
     df = pd.DataFrame(input_data_list)
     df.fillna(0, inplace=True)
     df['actions'] = df['actions'].astype(int)
-    df.to_excel('test.xlsx')
+    df['clicks'] = df['clicks'].astype(int)
+    df['impressions'] = df['impressions'].astype(int)
+    df['spend'] = df['spend'].astype(float)
+    df['ctr'] = df['ctr'].astype(float)
+    df.to_excel('test.xlsx', index=False)
 
