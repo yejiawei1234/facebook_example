5ae314c114ac069b76c553f756a92e288bf4f474
yejiawei1234 <yejiawei123@gmail.com>
2018-07-24 10:28:56 +0800

    del useless import

--- fb_account_data.py
+++ fb_account_data.py
@@ -5,22 +5,17 @@ Spyder Editor
 This is a temporary script file.
 """
 
-﻿from facebook_business import FacebookSession
+from facebook_business import FacebookSession
 from facebook_business import FacebookAdsApi
 from facebook_business.api import FacebookAdsApiBatch
-from facebook_business.adobjects.campaign import Campaign as AdCampaign
 from facebook_business.adobjects.adaccountuser import AdAccountUser as AdUser
-from facebook_business.adobjects.adaccount import AdAccount
 import json
 import os
 import pandas as pd
 from datetime import date, timedelta
-import click
-import pprint
 import time
-from functools import partial
 
-pp = pprint.PrettyPrinter(indent=4)
+
 this_dir = os.path.dirname(__file__)
 config_filename = os.path.join(this_dir, 'config.json')
 with open(config_filename) as config_file:
@@ -66,20 +61,28 @@ def generate_batches(iterable, batch_size_limit):
         yield batch
 
 
+def deal_df(input_data_list):
+    df = pd.DataFrame(input_data_list)
+    df.fillna(0, inplace=True)
+    df['actions'] = df['actions'].astype(int)
+    df['clicks'] = df['clicks'].astype(int)
+    df['impressions'] = df['impressions'].astype(int)
+    df['spend'] = df['spend'].astype(float)
+    df['ctr'] = df['ctr'].astype(float)
+    df.to_excel('test.xlsx', index=False)
+
+
 if __name__ == '__main__':
     start = time.time()
     FacebookAdsApi.set_default_api(api)
     me = AdUser(fbid='me')
     input_data_list = []
     my_accounts_list = list(me.get_ad_accounts())
-    fields = ['account_id',
-              'account_name',
-              'impressions',
-              'clicks',
-              'spend',
-              'ctr',
-              'actions']
+
+    fields = ['account_id', 'account_name', 'impressions', 'clicks', 'spend', 'ctr', 'actions']
+
     params = {'date_preset': 'yesterday'}
+
     to_do_list = []
 
 
@@ -94,12 +97,8 @@ if __name__ == '__main__':
 
     for accounts in generate_batches(my_accounts_list, 25):
         api_batch = FacebookAdsApiBatch(api)
-
         for account in accounts:
-            account_insight = account.get_insights(fields=fields,
-                                                   params=params,
-                                                   batch=api_batch)
-
+            account_insight = account.get_insights(fields=fields, params=params, batch=api_batch)
             api_batch.add_request(account_insight, success=add_to_list, failure=print_to)
         api_batch.execute()
 
@@ -109,14 +108,6 @@ if __name__ == '__main__':
             data_dict = deal_insight(data)
             input_data_list.append(data_dict)
 
-    df = pd.DataFrame(input_data_list)
-    df.fillna(0, inplace=True)
-    df['actions'] = df['actions'].astype(int)
-    df['clicks'] = df['clicks'].astype(int)
-    df['impressions'] = df['impressions'].astype(int)
-    df['spend'] = df['spend'].astype(float)
-    df['ctr'] = df['ctr'].astype(float)
-    df.to_excel('test.xlsx', index=False)
+    deal_df(input_data_list)
     end = time.time()
     print(f"{end - start:0.2f}")
-
